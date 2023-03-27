b18a1ca8fb61df0e8b10e6f3da66f5eefd6826af
mahbub96 <mahbub.alam.sobuz@gmail.com>
2023-03-27 17:16:47 +0600

    something change on DB

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -6,8 +6,115 @@ import ThreeDots from "../ThreeDots";
 import Skill from "./Skill.jsx";
 
 function Skills() {
-  const { data } = useFirestore();
+  const { data, setDocuments } = useFirestore();
   const { auth } = useDataContex();
+
+  setDocuments("Skills", {
+    data: [
+      {
+        id: 100,
+        name: "PHP",
+        src: "/assets/img/php.png",
+      },
+      {
+        id: 118,
+        name: "JavaScript",
+        src: "/assets/img/javaScript.png",
+      },
+      {
+        id: 101,
+        name: "React JS",
+        src: "/assets/img/javaScript.png",
+      },
+      {
+        id: 116,
+        name: "React Native",
+        src: "/assets/img/react-native.png",
+      },
+      {
+        id: 102,
+        name: "Vue JS",
+        src: "/assets/img/vue`.png",
+      },
+      {
+        id: 103,
+        name: "jQuery",
+        src: "/assets/img/jquery.png",
+      },
+      {
+        id: 104,
+        name: "Node Js",
+        src: "/assets/img/nodejs.png",
+      },
+      {
+        id: 105,
+        name: "MY SQL",
+        src: "/assets/img/mysql.png",
+      },
+      {
+        id: 106,
+        name: "git",
+        src: "/assets/img/git.png",
+      },
+      {
+        id: 107,
+        name: "github",
+        src: "/assets/img/github.png",
+      },
+      {
+        id: 108,
+        name: "CSS",
+        src: "/assets/img/css.png",
+      },
+      {
+        id: 109,
+        name: "Bootstarp",
+        src: "/assets/img/bootstarp.png",
+      },
+      {
+        id: 110,
+        name: "tailwind",
+        src: "/assets/img/tailwind.png",
+      },
+      {
+        id: 111,
+        name: "python",
+        src: "/assets/img/python.png",
+      },
+      {
+        id: 112,
+        name: "C Plus Plus",
+        src: "/assets/img/c++.png",
+      },
+      {
+        id: 113,
+        name: "C",
+        src: "/assets/img/c.png",
+      },
+      {
+        id: 114,
+        name: "Assembly Language",
+        src: "/assets/img/asm.png",
+      },
+
+      {
+        id: 115,
+        name: "SQlite3",
+        src: "/assets/img/sqlite3.png",
+      },
+
+      {
+        id: 117,
+        name: "Java",
+        src: "/assets/img/java.png",
+      },
+      {
+        id: 118,
+        name: "Swings",
+        src: "/assets/img/swings.png",
+      },
+    ],
+  });
 
   const { Skills } = data;
   const [modalShow, setModalShow] = React.useState(false);
