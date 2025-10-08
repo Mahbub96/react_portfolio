f6d22b71662c58c0dc20e609a7ca6b15d763b88b
Md Mahbub Alam <mahbub.alam.sobuz@gmail.com>
2025-10-08 11:17:52 +0600

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,24 @@
 module.exports = {
   apps: [
     {
-      name: "next-app",
-      script: "node .next/standalone/server.js",
+      name: "mahbub.dev",
+      script: "node",
+      args: ".next/standalone/server.js",
+      cwd: "/var/www/html/mahbub.dev",
+      instances: 1, // or 'max' if you want PM2 to auto-scale by CPU cores
+      exec_mode: "fork", // keeps it simple and stable for Next.js standalone
+      max_memory_restart: "512M", // restarts automatically if memory exceeds 512 MB
       env: {
         NODE_ENV: "production",
-        MONGODB_URI:
-          "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.8",
+        PORT: 3000,
+        NODE_OPTIONS: "--max-old-space-size=512",
+        MONGODB_URI: "mongodb://127.0.0.1:27017/portfolio?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.8"
       },
-    },
-  ],
+      error_file: "/var/www/html/mahbub.dev/logs/error.log",
+      out_file: "/var/www/html/mahbub.dev/logs/out.log",
+      merge_logs: true,
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      restart_delay: 5000
+    }
+  ]
 };
